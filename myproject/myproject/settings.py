b42e2dfc91ae2e6d6543124c3a86ec48679824a1
Nikita Mikhailov <mikhailoffnikita2016@yandex.ru>
2025-04-02 00:17:45 +0300

    update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'taggit',
     'widget_tweaks',
     'django_jinja',
-    # 'debug_toolbar',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -54,8 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 INTERNAL_IPS = [
